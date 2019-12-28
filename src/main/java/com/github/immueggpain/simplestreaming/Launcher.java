1653d8675d43a835c65cec8f273bc63cdb8d493a
Immueggpain <Immueggpain@users.noreply.github.com>
2019-12-28 22:05:14 +0800

    Update Launcher.java

--- src/main/java/com/github/immueggpain/simplestreaming/Launcher.java
+++ src/main/java/com/github/immueggpain/simplestreaming/Launcher.java
@@ -6,11 +6,11 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
 
-@Command(description = "Toolkit written in java.", name = "javatool", mixinStandardHelpOptions = true,
+@Command(description = "Streaming with OBS", name = "simple-streaming", mixinStandardHelpOptions = true,
 		version = Launcher.VERSTR, subcommands = { HelpCommand.class, BMPUDPHub.class, BMPPeer.class })
 public class Launcher implements Callable<Void> {
 
-	public static final String VERSTR = "0.4.6";
+	public static final String VERSTR = "0.0.1";
 	public static final int LOCAL_PORT = 2233;
 	public static final int LOCAL_OVPN_PORT = 1194;
 
